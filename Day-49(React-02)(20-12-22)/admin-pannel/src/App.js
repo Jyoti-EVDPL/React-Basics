ce555bea613edfd03a19e89ef77014ff6e1908ae
jpRealSpartan <121219411+jpRealSpartan@users.noreply.github.com>
2022-12-22 14:15:23 +0530

    Add files via upload

---
+++ Day-49(React-02)(20-12-22)/admin-pannel/src/App.js
@@ -0,0 +1,144 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// class App2 extends React.components {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <p>
+//             Edit <code>src/App.js</code> and save to reload.
+//           </p>
+//           <a
+//             className="App-link"
+//             href="https://reactjs.org"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             Learn React
+//           </a>
+//         </header>
+//       </div>
+//     );
+//   }
+// }
+
+// function App() {
+// return React.createElement('div', null,
+//   React.createElement('h1', null, 'Hyy'),
+//   React.createElement('h1', null, 'Hyy'),
+//   React.createElement('h1', null, 'Hyy'),
+// )
+//     return (
+//       <h1>Hyy</h1>
+//     );
+// }
+
+// function App() {
+//   const buttonClick=()=>{
+//     console.log('clicked');
+//   }
+//   return (
+//     <React.Fragment>
+//       <div>
+//         <button onClick={buttonClick} className="">Submit</button>
+//         <button onClick={buttonClick} className="">Apply Changes</button>
+//       </div>
+//     </React.Fragment>
+//   );
+// }
+//------------------------------------------------------------------------------------------------
+
+function App() {
+  let isSubmitted = false;
+
+  const onOnceApply = (message = "") => {
+    console.log('from Once clicked', message,isSubmitted);
+    isSubmitted = true;
+  }
+  return (
+    <React.Fragment>
+      <div>
+        <SubmitButton label="Submit" />
+        <SubmitButton label="Apply" onCompmete={onOnceApply} />
+      </div>
+      <Navbar/>
+    </React.Fragment>
+  );
+}
+
+// function SubmitButton({ label, onCompmete }) {
+function SubmitButton(props) {
+  const handleClick = () => {
+    console.log('clicked');
+    if (props.onCompmete) {
+      props.onCompmete('This is done');
+    }
+  }
+  return (
+    <button onClick={handleClick} className="">{props.label}</button>
+  );
+}
+
+//-------------------------------------------------------------------------
+//CLASS BASED COMPONENT EXPLANATION
+
+class App2 extends React.Component {
+  onOnceApply = (message = "") => {
+    console.log('Apply button is clicked', message,this.isSubmitted);
+    this.isSubmitted = true;
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <div>
+        <SubmitButton2 label="Submit" />
+        <SubmitButton2 label="Apply" onComplete={this.onOnceApply} />
+        </div>
+      </React.Fragment>
+    )
+  }
+}
+
+class SubmitButton2 extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  handleClick = () => {
+    console.log('clicked');
+    if (this.props.onComplete) {
+      this.props.onComplete('This clicked is done');
+    }
+  }
+  render() {
+    return (
+      <button onClick={this.handleClick} className="">{this.props.label}</button>
+    );
+  }
+}
+
+export default App;
